16a4265c3b747fa76d9a9b12ba32955c1302c4a5
ShinobuFormula <33146121+ShinobuFormula@users.noreply.github.com>
2022-07-30 19:17:37 +0200

    fight mode action WIP

--- controller/communication.js
+++ controller/communication.js
@@ -14,16 +14,26 @@ const comm = (io) => {
 			playerInit(playerID, socket.id);
 			const match = matchmake.addPlayer(playerID);
 			if (match) {
-				const playerInfo = fightmode.ready(match);
-				playerInfo.forEach((player) => {
-					io.to(playerSockets[player.id]).emit("gongue", player);
-				});
+				const [playerInfo, fightID] = fightmode.ready(match);
+				_socketTo(playerInfo, "gongue", { playerInfo, fightID });
 			} else socket.emit("matchmaking-pending");
+		});
+
+		socket.on("actions", (actions, playerID, fightID) => {
+			const [playerInfo] = fightmode.waitActions(actions, playerID, fightID);
+			if (res) _socketTo(playerInfo, "action-done", playerInfo);
+			else socket.emit("action-pending");
 		});
 	});
 
 	const playerInit = (playerID, socketID) => {
 		if (playerID && socketID) playerSockets[playerID.toString()] = socketID;
+	};
+
+	const _socketTo = (playerInfo, emit, data) => {
+		playerInfo.forEach((player) => {
+			io.to(playerSockets[player.id]).emit(emit, data);
+		});
 	};
 };
 
--- controller/fight.js
+++ controller/fight.js
@@ -21,27 +21,97 @@ const launchFight = (playersInfo) => {
 };
 
 const getTeam = (playerID) => {
-	return [{ dd: "d" }, { ddd: "d545" }];
+	return [
+		{
+			id: "1",
+			name: "ronkarétoal",
+			typeName: {
+				firstType: "fire",
+				secondType: "mental",
+			},
+			stats: {
+				hp: 100,
+				attack: 100,
+				def: 100,
+				speed: 100,
+				// precision: 100,
+				// statusRes: 100,
+				stamina: 100,
+				balance: 100,
+			},
+			passifId: 2,
+			skills: [1, 2, 3, 4],
+			status: [],
+			buff: [],
+		},
+		{
+			id: "2",
+			name: "étoalronkaré",
+			typeName: {
+				firstType: "fire",
+				secondType: "mental",
+			},
+			stats: {
+				hp: 100,
+				attack: 100,
+				def: 100,
+				speed: 100,
+				// precision: 100,
+				// statusRes: 100,
+				stamina: 100,
+				balance: 100,
+			},
+			passifId: 2,
+			skills: [5, 6, 7, 8],
+			status: [],
+			buff: [],
+		},
+	];
 };
 
 const fight = () => {
-	let instance = [];
-	let players = [];
+	let instance = {};
 
 	const ready = (match) => {
 		match.map((player) => {
 			player["team"] = getTeam(player.id);
+			player["actions"] = [];
 		});
 
 		return _startCombat(match);
 	};
 
-	const _startCombat = (match) => {
-		players.push(match);
-		return match;
+	const _getNewFightId = () => {
+		return "fid_" + Date.now().toString();
 	};
 
-	return { ready };
+	const _startCombat = (match) => {
+		const fightId = _getNewFightId();
+		instance[fightId.toString()] = match;
+		return [match, fightId];
+	};
+
+	const waitActions = (actions, playerID, fightID) => {
+		let playerActions = 0;
+		const tempInstance = instance[fightID];
+		if (!tempInstance) return null;
+		const tempTeamArray = tempInstance.filter(
+			(player) => player.id == playerID
+		);
+		if (tempTeamArray.length < 0) return null;
+		const tempPlayer = tempTeamArray[0];
+		tempPlayer.actions = actions;
+		tempInstance.forEach((player) => {
+			if (player.actions.length > 0) playerActions++;
+		});
+
+		if (playerActions >= 2) return _doActions(tempInstance); //action
+		else return null;
+	};
+
+	const _doActions = (instance) => {};
+
+	return { ready, waitActions };
 };
 
 export default fight;
