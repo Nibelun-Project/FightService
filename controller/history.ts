1b2981e16543a650d2035e3a3c84cae2fc189a12
ShinobuFormula <33146121+ShinobuFormula@users.noreply.github.com>
2025-02-15 22:59:58 +0100

    format/indentation space 4

--- controller/history.ts
+++ controller/history.ts
@@ -1,11 +1,11 @@
 import { actionInterface } from "../interfaces/action.js";
 import {
-  fightInfoInterface,
-  historyActionInterface,
-  historyContextEnum,
-  historyInterface,
-  historyMonsterInterface,
-  historySkillInterface,
+    fightInfoInterface,
+    historyActionInterface,
+    historyContextEnum,
+    historyInterface,
+    historyMonsterInterface,
+    historySkillInterface,
 } from "../interfaces/history.js";
 import { instanceInterface } from "../interfaces/instance.js";
 import { MonsterFightingInterface } from "../interfaces/monster.js";
@@ -13,72 +13,72 @@ import { SkillInterface } from "../interfaces/skill.js";
 
 const HISTORY_NAME = "fightInfo";
 const initFightInfo = (): fightInfoInterface => {
-  return {
-    round: 0,
-    history: [],
-    endgame: false,
-  };
+    return {
+        round: 0,
+        history: [],
+        endgame: false,
+    };
 };
 
 const initHistoryRound = (instance: instanceInterface) => {
-  instance[HISTORY_NAME].history[instance[HISTORY_NAME].round] = [];
-  instance[HISTORY_NAME].round++;
+    instance[HISTORY_NAME].history[instance[HISTORY_NAME].round] = [];
+    instance[HISTORY_NAME].round++;
 };
 
 const convertMonsterToHistory = (
-  monster: MonsterFightingInterface
+    monster: MonsterFightingInterface
 ): historyMonsterInterface => {
-  return {
-    id: monster.id,
-    name: monster.name,
-    type: monster.type,
-    playerID: monster.playerID,
-  };
+    return {
+        id: monster.id,
+        name: monster.name,
+        type: monster.type,
+        playerID: monster.playerID,
+    };
 };
 
 const convertSkillToHistory = (
-  skill: SkillInterface
+    skill: SkillInterface
 ): historySkillInterface => {
-  return {
-    name: skill.name,
-    type: skill.type,
-    targetType: skill.targetType,
-  };
+    return {
+        name: skill.name,
+        type: skill.type,
+        targetType: skill.targetType,
+    };
 };
 
 const convertActionToHistory = (
-  action: actionInterface
+    action: actionInterface
 ): historyActionInterface => {
-  return {
-    sourceID: action.sourceID,
-    skill: convertSkillToHistory(action.skill),
-  };
+    return {
+        sourceID: action.sourceID,
+        skill: convertSkillToHistory(action.skill),
+    };
 };
 
 const updateHistory = (
-  instance: instanceInterface,
-  update: historyInterface
+    instance: instanceInterface,
+    update: historyInterface
 ) => {
-  let fightInfo = instance[HISTORY_NAME];
-  if (
-    update.context !== historyContextEnum.SPEEDCONTEST ||
-    fightInfo.history[fightInfo.round - 1].every((event) => {
-      return !(
-        (event.content.monstersID[0] === update.content.monstersID[0] ||
-          event.content.monstersID[1] === update.content.monstersID[0]) &&
-        (event.content.monstersID[0] === update.content.monstersID[1] ||
-          event.content.monstersID[1] === update.content.monstersID[1])
-      );
-    })
-  )
-    fightInfo.history[fightInfo.round - 1].push(update);
+    let fightInfo = instance[HISTORY_NAME];
+    if (
+        update.context !== historyContextEnum.SPEEDCONTEST ||
+        fightInfo.history[fightInfo.round - 1].every((event) => {
+            return !(
+                (event.content.monstersID[0] === update.content.monstersID[0] ||
+                    event.content.monstersID[1] === update.content.monstersID[0]) &&
+                (event.content.monstersID[0] === update.content.monstersID[1] ||
+                    event.content.monstersID[1] === update.content.monstersID[1])
+            );
+        })
+    )
+        fightInfo.history[fightInfo.round - 1].push(update);
 };
 
 export {
-  initFightInfo,
-  initHistoryRound,
-  updateHistory,
-  convertMonsterToHistory,
-  convertSkillToHistory,
-  convertActionToHistory,
+    initFightInfo,
+    initHistoryRound,
+    updateHistory,
+    convertMonsterToHistory,
+    convertSkillToHistory,
+    convertActionToHistory,
 };
--- controller/monsterStat.ts
+++ controller/monsterStat.ts
@@ -1,54 +1,54 @@
 import { instanceInterface } from "../interfaces/instance.js";
 import {
-  MonsterFightingInterface,
-  monsterStatsEnum,
-  statsConst,
+    MonsterFightingInterface,
+    monsterStatsEnum,
+    statsConst,
 } from "../interfaces/monster.js";
 import { isOnBoard } from "./instance.js";
 
 const staminaRefill = (instance: instanceInterface) => {
-  instance.players.forEach((player) => {
-    player.onBoard.forEach((monster) => {
-      refillStat()[monsterStatsEnum.STAMINA](
-        monster,
-        statsConst.STAMINAREFILLONBOARD
-      );
+    instance.players.forEach((player) => {
+        player.onBoard.forEach((monster) => {
+            refillStat()[monsterStatsEnum.STAMINA](
+                monster,
+                statsConst.STAMINAREFILLONBOARD
+            );
+        });
+        player.team
+            .filter((monster) => !isOnBoard(instance, monster.id))
+            .forEach((monster) => {
+                refillStat()[monsterStatsEnum.STAMINA](
+                    monster,
+                    statsConst.STAMINAREFILLNOTONBOARD
+                );
+            });
     });
-    player.team
-      .filter((monster) => !isOnBoard(instance, monster.id))
-      .forEach((monster) => {
-        refillStat()[monsterStatsEnum.STAMINA](
-          monster,
-          statsConst.STAMINAREFILLNOTONBOARD
-        );
-      });
-  });
 };
 
 const refillStat = () => {
-  const balance = (monster: MonsterFightingInterface, value: number) => {
-    if (monster.stats.balance !== monster.starting.balance) {
-      monster.stats.balance += value;
-      if (monster.stats.balance > monster.starting.balance)
-        monster.stats.balance = monster.starting.balance;
-    }
-  };
-  const hp = (monster: MonsterFightingInterface, value: number) => {
-    if (monster.stats.hp !== monster.starting.hp) {
-      monster.stats.hp += value;
-      if (monster.stats.hp > monster.starting.hp)
-        monster.stats.hp = monster.starting.hp;
-    }
-  };
-  const stamina = (monster: MonsterFightingInterface, value: number) => {
-    if (monster.stats.stamina !== monster.starting.stamina) {
-      monster.stats.stamina += value;
-      if (monster.stats.stamina > monster.starting.stamina)
-        monster.stats.stamina = monster.starting.stamina;
-    }
-  };
+    const balance = (monster: MonsterFightingInterface, value: number) => {
+        if (monster.stats.balance !== monster.starting.balance) {
+            monster.stats.balance += value;
+            if (monster.stats.balance > monster.starting.balance)
+                monster.stats.balance = monster.starting.balance;
+        }
+    };
+    const hp = (monster: MonsterFightingInterface, value: number) => {
+        if (monster.stats.hp !== monster.starting.hp) {
+            monster.stats.hp += value;
+            if (monster.stats.hp > monster.starting.hp)
+                monster.stats.hp = monster.starting.hp;
+        }
+    };
+    const stamina = (monster: MonsterFightingInterface, value: number) => {
+        if (monster.stats.stamina !== monster.starting.stamina) {
+            monster.stats.stamina += value;
+            if (monster.stats.stamina > monster.starting.stamina)
+                monster.stats.stamina = monster.starting.stamina;
+        }
+    };
 
-  return { balance, hp, stamina };
+    return { balance, hp, stamina };
 };
 
 export { staminaRefill, refillStat };
